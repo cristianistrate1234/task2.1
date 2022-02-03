cbf3c3929d22abcd724dace4180da9623b1023a0
Cristi2002 <cristianistrate1234@gmail.com>
2022-02-03 22:22:31 +0300

    noi modificari

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
-submit.disabled = true;
+
+let usernameField = false;
+let emailField = false;
+let firstPasswordField = false;
+let secondPasswordField = false;
 
 username.addEventListener('input', e => {
     e.preventDefault();
@@ -22,63 +26,60 @@ password2.addEventListener('input', e => {
     checkInputsPassword2();
 });
 
-
-
-submit.addEventListener('change' , () =>{
-    checkInputs()
-})
-function checkInputs() {
-    const usernameValue = username.value.trim();
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
-
-    if(usernameValue.length >= 3 && passwordValue === password2Value && passwordValue.length >= 6 && isEmail(emailValue)) {
-        submit.disabled = false;
-    } else {
-        submit.disabled = true;
-    }
-}
 function checkInputsUsername() {
     const usernameValue = username.value.trim();
 
-    if(usernameValue.length < 3 ) {
-        setErrorFor(username, 'Minim 3 caractere');
+    if(usernameValue.length < 3) {
+        setErrorFor(username, 'Minim 3 caractere || Maxim 12 caractere');
+        usernameField = false;
     } else {
         setSuccessFor(username);
+        usernameField = true;
     }
+    enableSubmit()
 }
 function checkInputsEmail() {
     const emailValue = email.value.trim();
 
-    if (emailValue === '') {
+    if (!emailValue.length) {
         setErrorFor(email, 'Acesta nu este un email');
+        emailField = false;
     } else if (!isEmail(emailValue)) {
         setErrorFor(email, 'Email invalid');
+        emailField = false;
     } else {
         setSuccessFor(email);
+        emailField = true;
     }
+    enableSubmit()
 }
 function checkInputsPassword() {
     const passwordValue = password.value.trim();
 
     if (passwordValue.length < 6) {
         setErrorFor(password, 'Minim 6 caractere');
+        firstPasswordField = false;
     } else {
         setSuccessFor(password);
+        firstPasswordField = true;
     }
+    enableSubmit()
 }
 function checkInputsPassword2() {
     const password2Value = password2.value.trim();
     const passwordValueConfirm = password.value.trim();
 
-    if (password2Value === '') {
+    if (!password2Value.length) {
         setErrorFor(password2, 'Confirma parola');
+        secondPasswordField = false;
     } else if (passwordValueConfirm !== password2Value) {
         setErrorFor(password2, 'Parolele nu coincid');
+        secondPasswordField = false;
     } else {
         setSuccessFor(password2);
+        secondPasswordField = true;
     }
+    enableSubmit()
 }
 function setErrorFor(input, message) {
     const formControl = input.parentElement;
@@ -92,4 +93,10 @@ function setSuccessFor(input) {
 }
 function isEmail(email) {
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+function enableSubmit(){
+    if (usernameField === true && emailField === true && firstPasswordField === true && secondPasswordField === true){
+        submit.style.backgroundColor = "dodgerblue";
+    }
 }
